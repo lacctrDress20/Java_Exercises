8f8e509cda571869a723d74e770a6f551a4afb6a
lacctrDress20 <1490244@senati.pe>
2023-03-24 09:43:23 -0500

    Muestra de un grafico sencillo

---
+++ hola.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(string[] args) {
+        System.out.print("    j       a   v       v   a");
+        System.out.print("    j      a a   v     v   a");
+        System.out.print("  j  j    aaaaa   v   v   a");
+        System.out.print("   jj     a   a    vv   a");
+
+    }
+}
