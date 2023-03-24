c6af93370cb7aed62d6d7d4de774ab5577d39d74
lacctrDress20 <1490244@senati.pe>
2023-03-24 11:09:12 -0500

    Creacion de un programa que evalue si es mnor o igual o diferente que..

--- Ejercicios_hoy_24/hola.java
+++
@@ -1,9 +0,0 @@
-public class main{
-    public static void main(string[] args) {
-        System.out.print("    j       a   v       v   a");
-        System.out.print("    j      a a   v     v   a a");
-        System.out.print("  j  j    aaaaa   v   v   aaaaa");
-        System.out.print("   jj     a   a    vv     a   a");
-
-    }
-}
---
+++ numMenorQue.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class numMenorQue { 
+    public static void main(string[] args) {
+        Scanner num1 = new Scanner(System.in);
+        System.out.println("ingrese el primer numero: ");
+        double numero = num1.nextDouble();
+        if(numero != 20){
+            System.out.println("El numero es diferente a 20");
+        }if(numero <=20){
+            System.ou.println("El numero es menor que 20");
+        }if(numero >=20){
+            System.out.println("El numero es menor o igual que 20")
+        }
+
+    
+    }
+}    
